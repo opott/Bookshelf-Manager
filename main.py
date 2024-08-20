28f9f1131691e84711c9833ba72062f9051081bb
Oliver P. <GitHub@opott.is-cool.me>
2024-08-20 15:27:46 +0000

    feat: Add functionality to search and print book records by ISBN

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from pyairtable import Api
+from pyairtable.formulas import match
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,4 +8,27 @@ load_dotenv()
 api = Api(os.getenv('AIRTABLE_PAT'))
 table = api.table(os.getenv('BASE_ID'), os.getenv('TABLE_ID'))
 
-print(table.all())
+def main():
+    print("""
+    Welcome to Bookshelf Manager.
+    What would you like to do?
+
+    (f)etch a book record  
+          """)
+    
+    choice  = input("> ")
+
+    if choice == "f":
+        isbn = input("Please enter the ISBN number of the book you are looking for: ")
+
+        searchformula = match({"ISBN": isbn})
+        records = table.all(formula=searchformula)
+
+        for record in records:
+            print("ISBN: " + record['fields']['ISBN']['text'])
+            print("Title: " + record['fields']['Title'])
+            print("Author: " + record['fields']['Author'])
+    else:
+        print("Invalid Selection")
+
+main()
